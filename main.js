168d4a2f230281772152d96967c654f264ca085a
dinhce <dinhluckyboy@gmail.com>
2026-02-13 16:40:25 +0700

    update 21.19

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// alert("thong bao");
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+$("#btn-model").onclick = function () {
+  $("#model").classList.add("show");
+};
+
+$(".model-close").onclick = function () {
+  $("#model").classList.remove("show");
+};
+
+$("#model").onclick = function (e) {
+  if (e.target === $("#model")) {
+    $("#model").classList.remove("show");
+  }
+};
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    $("#model").classList.remove("show");
+    console.log(e);
+  }
+});
