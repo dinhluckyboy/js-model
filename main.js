60fccb3e6bf02a4eaf62d751e76540e669cd5078
dinhce <dinhluckyboy@gmail.com>
2026-03-01 17:48:14 +0700

    done 21.37

--- main.js
+++ main.js
@@ -5,208 +5,212 @@ Model.elements = []; // array save model open current
 
 function Model(option = {}) {
   // get content... from option
-  const {
-    templateId,
-    closeMethods = ["button", "overlay", "escape"],
-    destroyOnClose = true,
-    cssClass = [],
-    onOpen,
-    onClose,
-    footer = false,
-  } = option;
-  const template = $(`#${templateId}`);
-  if (!template) {
+  this.opt = Object.assign(
+    {
+      //  templateId,
+      closeMethods: ["button", "overlay", "escape"],
+      destroyOnClose: true,
+      cssClass: [],
+      // onOpen,
+      // onClose,
+      footer: false,
+    },
+    option
+  );
+
+  this.template = $(`#${this.opt.templateId}`);
+  if (!this.template) {
     console.error("template id none exits");
     return;
   }
-  this._allowButtonClose = closeMethods.includes("button");
-  this._allowBackdropClose = closeMethods.includes("overlay");
-  this._allowEscapeClose = closeMethods.includes("escape");
+  this._allowButtonClose = this.opt.closeMethods.includes("button");
+  this._allowBackdropClose = this.opt.closeMethods.includes("overlay");
+  this._allowEscapeClose = this.opt.closeMethods.includes("escape");
 
-  this._createElement = () => {
-    const content = template.content.cloneNode(true);
-    // create element
-    this._backdrop = document.createElement("div");
-    this._backdrop.className = "model-backdrop";
+  this._footerButton = [];
+}
 
-    const container = document.createElement("div");
-    container.className = "model-container";
+// -----------------
 
-    if (cssClass.length > 0) {
-      cssClass.forEach((className) => {
-        if (typeof className === "string") {
-          container.classList.add(className);
-        }
-      });
-    } // add css class to container
+Model.prototype._createElement = function () {
+  const content = this.template.content.cloneNode(true);
+  // create element
+  this._backdrop = document.createElement("div");
+  this._backdrop.className = "model-backdrop";
 
-    if (this._allowButtonClose) {
-      const btnClose = this._createButton("&times;", "model-close", this.close);
-      container.append(btnClose); // append element
-    }
+  const container = document.createElement("div");
+  container.className = "model-container";
 
-    const modelContent = document.createElement("div");
-    modelContent.className = "model-content";
+  if (this.opt.cssClass.length > 0) {
+    this.opt.cssClass.forEach((className) => {
+      if (typeof className === "string") {
+        container.classList.add(className);
+      }
+    });
+  } // add css class to container
 
-    // append element
-    modelContent.append(content);
-    container.append(modelContent);
-    this._backdrop.append(container);
-    document.body.append(this._backdrop);
+  if (this._allowButtonClose) {
+    const btnClose = this._createButton("&times;", "model-close", () =>
+      this.close()
+    );
+    container.append(btnClose); // append element
+  }
 
-    // add footer
-    if (footer) {
-      this._modelFooter = document.createElement("div");
-      this._modelFooter.className = "model-footer";
-      this._renderFooterContent(); // render footer content
-      this._renderButton(); // render button to footer
-      container.append(this._modelFooter); // add footer to container
-    }
-  };
+  const modelContent = document.createElement("div");
+  modelContent.className = "model-content";
 
-  // -
-  this.open = () => {
-    Model.elements.push(this); // push model open current
+  // append element
+  modelContent.append(content);
+  container.append(modelContent);
+  this._backdrop.append(container);
+  document.body.append(this._backdrop);
 
-    if (!this._backdrop) {
-      this._createElement();
-    }
+  // add footer
+  if (this.opt.footer) {
+    this._modelFooter = document.createElement("div");
+    this._modelFooter.className = "model-footer";
+    this._renderFooterContent(); // render footer content
+    this._renderButton(); // render button to footer
+    container.append(this._modelFooter); // add footer to container
+  }
+};
 
-    // show backdrop
-    setTimeout(() => {
-      this._backdrop.classList.add("show");
-    }, 0);
+Model.prototype.open = function () {
+  Model.elements.push(this); // push model open current
 
-    // handel close event
-    if (this._allowBackdropClose) {
-      this._backdrop.onclick = (e) => {
-        if (e.target === this._backdrop) {
-          this.close();
-        }
-      };
-    }
+  if (!this._backdrop) {
+    this._createElement();
+  }
 
-    // handel escape close
-    this._handelEscapeClose = (e) => {
-      const lastModel = Model.elements[Model.elements.length - 1]; // last model
-      if (e.key === "Escape" && this === lastModel) {
+  // show backdrop
+  setTimeout(() => {
+    this._backdrop.classList.add("show");
+  }, 0);
+
+  // handel close event
+  if (this._allowBackdropClose) {
+    this._backdrop.onclick = (e) => {
+      if (e.target === this._backdrop) {
         this.close();
       }
     };
+  }
 
-    if (this._allowEscapeClose) {
-      document.addEventListener("keydown", this._handelEscapeClose);
+  // handel escape close
+  this._handelEscapeClose = (e) => {
+    const lastModel = Model.elements[Model.elements.length - 1]; // last model
+    if (e.key === "Escape" && this === lastModel) {
+      this.close();
+    }
+  };
+
+  if (this._allowEscapeClose) {
+    document.addEventListener("keydown", this._handelEscapeClose);
+  }
+
+  // disable scroll
+  document.body.classList.add("no-scroll");
+  //padding right scroll bar
+  document.body.style.paddingRight = this._getScrollBar() + "px";
+
+  // onOpen
+  this._onTransitionEnd(this.opt.onOpen);
+
+  return this._backdrop;
+};
+
+Model.prototype.close = function (destroy = this.opt.destroyOnClose) {
+  if (this._allowEscapeClose) {
+    document.removeEventListener("keydown", this._handelEscapeClose);
+  } // remove event listener
+
+  Model.elements.pop(); // remove element last array
+
+  this._backdrop.classList.remove("show");
+
+  this._onTransitionEnd(() => {
+    this._backdrop.ontransitionend = null; // gỡ handel sau khi chạy xong
+
+    if (destroy && this._backdrop) {
+      this._backdrop.remove();
+      this._backdrop = null;
+      this._modelFooter = null;
     }
 
-    // disable scroll
-    document.body.classList.add("no-scroll");
-    //padding right scroll bar
-    document.body.style.paddingRight = this._getScrollBar() + "px";
+    // onClose
+    if (typeof this.opt.onClose === "function") {
+      this.opt.onClose();
+    }
+    if (!Model.elements.length) {
+      // enable scroll
+      document.body.classList.remove("no-scroll");
+      //remove padding right scroll bar
+      document.body.style.paddingRight = "";
+    } // Model.length = 0 thi show scroll bar
+  });
+};
 
-    // onOpen
-    this._onTransitionEnd(onOpen);
+Model.prototype.destroy = function () {
+  this.close(true);
+};
 
-    return this._backdrop;
+Model.prototype._onTransitionEnd = function (callback) {
+  this._backdrop.ontransitionend = (e) => {
+    if (e.propertyName !== "transform") return;
+    if (typeof callback === "function") {
+      callback();
+    }
   };
+};
 
-  // tinh do rong cua scroll bar
-  this._getScrollBar = () => {
-    if (this._scrollBarWidth !== undefined) return this._scrollBarWidth;
-    const div = document.createElement("div");
-    div.style.position = "absolute";
-    div.style.top = "-999px";
-    div.style.width = "100px";
-    div.style.height = "100px";
-    div.style.overflow = "scroll";
-    document.body.appendChild(div);
-    this._scrollBarWidth = div.offsetWidth - div.clientWidth;
-    document.body.removeChild(div);
-    return this._scrollBarWidth;
-  };
+Model.prototype._getScrollBar = function () {
+  if (this._scrollBarWidth !== undefined) return this._scrollBarWidth;
+  const div = document.createElement("div");
+  div.style.position = "absolute";
+  div.style.top = "-999px";
+  div.style.width = "100px";
+  div.style.height = "100px";
+  div.style.overflow = "scroll";
+  document.body.appendChild(div);
+  this._scrollBarWidth = div.offsetWidth - div.clientWidth;
+  document.body.removeChild(div);
+  return this._scrollBarWidth;
+};
 
-  // -
-  this.close = (destroy = destroyOnClose) => {
-    if (this._allowEscapeClose) {
-      document.removeEventListener("keydown", this._handelEscapeClose);
-    } // remove event listener
+Model.prototype.setFooterContent = function (html) {
+  this._footerContent = html;
+  this._renderFooterContent(); // render footer content
+};
 
-    Model.elements.pop(); // remove element last array
+Model.prototype.addFooterButton = function (title, className, callback) {
+  const btn = this._createButton(title, className, callback);
+  this._footerButton.push(btn);
+  this._renderButton(); // render button to footer
+};
 
-    this._backdrop.classList.remove("show");
+Model.prototype._createButton = function (title, className, callback) {
+  const btn = document.createElement("button");
+  btn.className = className;
+  btn.innerHTML = title;
+  btn.onclick = callback;
+  return btn;
+};
 
-    this._onTransitionEnd(() => {
-      this._backdrop.ontransitionend = null; // gỡ handel sau khi chạy xong
-
-      if (destroy && this._backdrop) {
-        this._backdrop.remove();
-        this._backdrop = null;
-        this._modelFooter = null;
-      }
-
-      // onClose
-      if (typeof onClose === "function") {
-        onClose();
-      }
-      if (!Model.elements.length) {
-        // enable scroll
-        document.body.classList.remove("no-scroll");
-        //remove padding right scroll bar
-        document.body.style.paddingRight = "";
-      } // Model.length = 0 thi show scroll bar
+Model.prototype._renderButton = function () {
+  if (this._footerButton.length > 0 && this._modelFooter) {
+    this._footerButton.forEach((btn) => {
+      this._modelFooter.append(btn);
     });
-  };
+  }
+}; // render button to footer
 
-  // -
-  this.destroy = () => {
-    this.close(true);
-  };
+Model.prototype._renderFooterContent = function () {
+  if (this._modelFooter && this._footerContent) {
+    this._modelFooter.innerHTML = this._footerContent;
+  }
+}; // render footer content
 
-  // -
-  this._onTransitionEnd = (callback) => {
-    this._backdrop.ontransitionend = (e) => {
-      if (e.propertyName !== "transform") return;
-      if (typeof callback === "function") {
-        callback();
-      }
-    };
-  };
-
-  this.setFooterContent = (html) => {
-    this._footerContent = html;
-    this._renderFooterContent(); // render footer content
-  };
-
-  this._footerButton = [];
-
-  this.addFooterButton = (title, className, callback) => {
-    const btn = this._createButton(title, className, callback);
-    this._footerButton.push(btn);
-    this._renderButton(); // render button to footer
-  };
-
-  this._createButton = (title, className, callback) => {
-    const btn = document.createElement("button");
-    btn.className = className;
-    btn.innerHTML = title;
-    btn.onclick = callback;
-    return btn;
-  };
-
-  this._renderButton = () => {
-    if (this._footerButton.length > 0 && this._modelFooter) {
-      this._footerButton.forEach((btn) => {
-        this._modelFooter.append(btn);
-      });
-    }
-  }; // render button to footer
-
-  this._renderFooterContent = () => {
-    if (this._modelFooter && this._footerContent) {
-      this._modelFooter.innerHTML = this._footerContent;
-    }
-  }; // render footer content
-}
-
+// ---------------
 // create model
 const model1 = new Model({
   templateId: "model-1",
